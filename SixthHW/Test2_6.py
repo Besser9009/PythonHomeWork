d79a1d98ba1ef2471bace0764bf747df9b67cc63
besser <besser9009@mail.ru>
2023-06-08 23:02:56 +0300

    add full Test2_6

--- SixthHW/Test2_6.py
+++ SixthHW/Test2_6.py
@@ -1,1 +1,12 @@
 #Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+import random
+array = []
+for i in range(25):
+    elem_array = random.randint(0, 100)
+    array.append(elem_array)
+min = int(input("Введите минимальный порог поиска: "))
+max = int(input("Введите максимальный порог поиска: "))
+print(array)
+for i in range(25):
+    if min <= array[i] <= max:
+        print(f"{array[i]}: {i}")
